46d57fb8e919141338a1a62d33a15b61acd62388
yasersharifi <yassersharifi74@gmail.com>
2021-09-23 12:51:18 +0330

    use axios for read data from file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="main-app">
-      <h1 class="text-success">{{title}}</h1>
-      <font-awesome-icon icon="plus" class="mx-2"/>add appointment
+  <div id="main-app" class="p-4">
+      <h1 class="text-success mb-3">{{title}}</h1>
+      <span class="bg-success text-light mb-3 d-inline-block" style="padding: 10px 15px; border-radius: 5px;">
+          <font-awesome-icon icon="plus" /> add appointment
+      </span>
 
       <div v-for="(item, index) in appointments" :key="index">
           <h4>{{item.petName}}</h4>
@@ -12,41 +14,23 @@
 
 <script>
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+import axios from "axios";
+
 export default {
   name: 'mainApp',
   data: function () {
       return {
-          title: "My Crud Project",
-          appointments: [
-              {
-                "petName": "Spot",
-                "petOwner": "Constance Smith",
-                "aptDate": "2017-07-24 08:30",
-                "aptNotes": "This German Shepherd is having some back pain"
-              },
-              {
-                "petName": "Goldie",
-                "petOwner": "Barot Bellingham",
-                "aptDate": "2017-07-22 15:50",
-                "aptNotes": "This Goldfish has some weird spots in the belly"
-              },
-              {
-                "petName": "Mitten",
-                "petOwner": "Hillary Goldwyn",
-                "aptDate": "2017-07-21 9:15",
-                "aptNotes": "Cat has excessive hairballs"
-              },
-              {
-                "petName": "Buffy",
-                "petOwner": "Hassum Harrod",
-                "aptDate": "2017-07-20 15:30",
-                "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
-              }
-          ]
+          title: "Appointments List",
+          appointments: "",
       }
   },
   components: {
       FontAwesomeIcon
+  },
+  mounted() {
+      axios.get("./data/appointments.json")
+      .then(response => (this.appointments = response.data))
   }
 }
 </script>
