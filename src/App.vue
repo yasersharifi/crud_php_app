f18810bd9fd461403ac69120339f0d50a026a382
yasersharifi <yassersharifi74@gmail.com>
2021-09-23 11:47:30 +0330

    install vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-app">
+      <h1 class="text-success">{{title}}</h1>
+      <font-awesome-icon icon="plus" class="mx-2"/>add appointment
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 export default {
-  name: 'App',
+  name: 'mainApp',
+  data: function () {
+      return {
+          title: "My Crud Project"
+      }
+  },
   components: {
-    HelloWorld
+      FontAwesomeIcon
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faPlus, faTrash, faEdit, faCheck} from "@fortawesome/free-solid-svg-icons";
+library.add(faPlus, faTrash, faEdit, faCheck);
+
 Vue.config.productionTip = false
 
 new Vue({
