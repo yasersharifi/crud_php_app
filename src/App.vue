5eabe29566b3a2b9eca146615db3a16c1c821847
yasersharifi <yassersharifi74@gmail.com>
2021-09-25 13:24:39 +0330

    delete data from svc file

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <template>
-  <div id="main-app" class="p-4">
-      <h1 class="text-success mb-3">{{title}}</h1>
-      <span class="bg-success text-light mb-3 d-inline-block" style="padding: 10px 15px; border-radius: 5px;">
-          <font-awesome-icon icon="plus" /> add appointment
-      </span>
-
-      <div v-for="(item, index) in appointments" :key="index">
-          <h4>{{item.petName}}</h4>
-          <p>{{item.aptNotes}}</p>
+  <div id="main-app" class="container">
+      <div class="row">
+          <appointment-lists :appointments="appointments" @remove="removeItem"/>
       </div>
   </div>
 </template>
 
 <script>
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-
+import AppointmentLists from "./components/AppointmentLists";
 import axios from "axios";
+import _ from "lodash";
 
 export default {
   name: 'mainApp',
@@ -26,11 +20,16 @@ export default {
       }
   },
   components: {
-      FontAwesomeIcon
+      AppointmentLists
   },
   mounted() {
       axios.get("./data/appointments.json")
       .then(response => (this.appointments = response.data))
+  },
+  methods: {
+      removeItem: function (apt) {
+          this.appointments = _.without(this.appointments, apt);
+      }
   }
 }
 </script>
---
+++ src/components/AppointmentLists.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="col-12 mt-4">
+        <div class="card">
+            <div class="card-body">
+                <div class="list-group list-group-flush">
+                    <div class="list-group-item d-flex align-items-start" v-for="(item, index) in appointments" :key="index">
+                        <button class="me-2 btn btn-sm btn-danger">
+                            <font-awesome-icon icon="trash" @click="$emit('remove', item)"/>
+                        </button>
+                        <div class="w-100">
+                            <div class="d-flex justify-content-between">
+                                <span class="h4 text-primary">{{item.petName}}</span>
+                                <span class="float-end">{{formattedDate(item.aptDate)}}</span>
+                            </div>
+                            <div class="owner-name">
+                                <span class="fw-bold text-primary me-1">Owner: </span>
+                                <span>{{item.petOwner}}</span>
+                            </div>
+                            <div>{{item.aptNotes}}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+    import moment from "moment";
+    export default {
+        name: "AppointmentLists",
+        props: ["appointments"],
+        components: {
+            FontAwesomeIcon,
+        },
+        methods: {
+          formattedDate: function (date) {
+              return moment(new Date(date)).format("MM-DD-YY, h:mm a")
+          }
+        }
+    }
+</script>
